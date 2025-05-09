d11e758834bb435f4a9e217cec182b5a4a5a47da
jthomporter <jonathanporter491@u.boisestate.edu>
2025-05-08 21:46:48 -0700

    Set the main.py file to put doormat against DunceFish.

--- main.py
+++ main.py
@@ -6,9 +6,9 @@ from DunceFishChessBot import DunceFishChessBot
 # bot = DoorMatChessBot()
 # bot = DunceFishChessBot(3)
 
-# white_bot = DoorMatChessBot()
-white_bot = DunceFishChessBot(1)
-black_bot = DunceFishChessBot(3)
+white_bot = DoorMatChessBot()
+# white_bot = DunceFishChessBot(1)
+black_bot = DunceFishChessBot(1)
 
 chess_game = ChessGame(white_bot, black_bot)
 chess_game.have_bots_play()
