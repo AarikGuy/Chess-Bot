7bcb41b84b88ea9dfb23b99d768c33cc10e5be9b
jthomporter <jthomporter@gmail.com>
2025-12-20 15:49:47 -0800

    Removed commented code

--- main.py
+++ main.py
@@ -3,13 +3,8 @@ from ChessGame import ChessGame
 from bots.DoorMatChessBot import DoorMatChessBot
 from bots.DunceFishChessBot import DunceFishChessBot
 
-# bot = DoorMatChessBot()
-# bot = DunceFishChessBot(3)
-
 white_bot = DoorMatChessBot()
 black_bot = DunceFishChessBot(ply_cutoff=1, random_move_frequency=5)
-# black_bot = DunceFishChessBot(ply_cutoff=2, random_move_frequency=5)
 
 chess_game = ChessGame(white_bot, black_bot)
 chess_game.have_bots_play()
-# chess_game.play_against_bot()
