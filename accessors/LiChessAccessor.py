1af676296a1ca2afbe7bf032dc91ccdf238cccca
Jonathan Porter <jthomporter@gmail.com>
2026-01-01 01:11:21 -0800

    Implemented accessor methods to interact with LiChess

--- accessors/LiChessAccessor.py
+++ accessors/LiChessAccessor.py
@@ -1,13 +1,13 @@
 import json
+import requests
 
 SECRETS_PATH = "secrets.json"
 LI_CHESS_TOKEN = "LiChessToken"
+LI_CHESS_BASE_URL = "https://lichess.org/"
 
 
 class LiChessAccessor:
     def __init__(self):
-        self.base_url = "TODO replace w/ base url"
-
         secrets = None
         with open("secrets.json") as f:
             secrets = json.load(f)
@@ -17,11 +17,44 @@ class LiChessAccessor:
 
         self.token = secrets[LI_CHESS_TOKEN]
 
-    def get_challenges_async(self):
-        pass
+    def get_challenges(self):
+        response = requests.get(
+            f"{LI_CHESS_BASE_URL}/api/challenge", headers={"Authorization": self.token}
+        )
 
-    def accept_challenge_async(self):
-        pass
+        if response.status_code != 200:
+            raise Exception("Unable to fetch challenges from LiChess.")
+
+        return response.content
+
+    def accept_challenge(self, challenge_id: str):
+        response = requests.post(
+            f"{LI_CHESS_BASE_URL}/api/challenge/{challenge_id}/accept",
+            headers={"Authorization": self.token},
+        )
+
+        if response.status_code != 200:
+            raise Exception(f"Unable to accept challenge {challenge_id}")
+
+    def decline_challenge(self, challenge_id):
+        response = requests.post(
+            f"{LI_CHESS_BASE_URL}/api/challenge/{challenge_id}/decline",
+            headers={"Authorization": self.token},
+            data={"reason": "generic"},
+        )
+
+        if response.status_code != 200:
+            raise Exception(f"Failed to decline challenge {challenge_id}")
+
+    def make_move(self, game_id: str, move: str, offering_draw: bool):
+        response = requests.post(
+            f"{LI_CHESS_BASE_URL}/api/bot/game/{game_id}/move/{move}",
+            headers={"Authorization": self.token},
+            data={"offeringDraw": offering_draw},
+        )
+
+        if response.status_code != 200:
+            raise Exception(f"Failed to push move {move} to game {game_id}")
 
     """
     Method that sends a message to the opposing player.
