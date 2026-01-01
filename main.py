ba4647fb55d6c5496809359703c3138f5c81da39
Jonathan Porter <jthomporter@gmail.com>
2026-01-01 12:39:27 -0800

    Cleared main.py file

--- main.py
+++ main.py
@@ -1,9 +1,2 @@
-from ChessGame import ChessGame
-from bots.DoorMatChessBot import DoorMatChessBot
-from bots.DunceFishChessBot import DunceFishChessBot
 
-white_bot = DunceFishChessBot(ply_cutoff=2)
-black_bot = DunceFishChessBot(ply_cutoff=1)
 
-chess_game = ChessGame(white_bot=white_bot, black_bot=black_bot)
-chess_game.have_bots_play()
